21848416948ce4d0c5ac67c0e306fe8ba39c5b4b
amyaitran <30616230+amyaitran@users.noreply.github.com>
2021-04-01 16:29:09 -0700

    added console log to submit button

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	var max_fields = 10;
-	var wrapper = $(".input_fields"); 
+	var wrapper = $(".input_fields");
 	var add_button = $(".add_field_button");
-	
+	var submit_button = $(".submit_btn");
+	var question = $("#question");
+
+
 	var x = 1;
-	console.log('ok');
-	$(add_button).click(function(e){ 
-		e.preventDefault();
-		if(x < max_fields){ 
-			x++; 
-			$(wrapper).append('<div><input type="text" class="form-container" name="answer"/><a href="#" class="remove_field">Remove</a></div>');
+	console.log('ojjjk');
+	$(add_button).click(function (e) {
+		console.log(e);
+		if (x < max_fields) {
+			x++;
+			$(wrapper).append('<div><input type="text" class="form-container answer"><a href="#" class="remove_field">Remove</a></div>');
 		}
 	});
-	
-	$(wrapper).on("click",".remove_field", function(e){ 
-		e.preventDefault(); $(this).parent('div').remove(); x--;
+
+	$(wrapper).on("click", ".remove_field", function (e) {
+		$(this).parent('div').remove(); x--;
 	})
+
+
+
+	$(submit_button).click(function (e) {
+		console.log('question: '+question[0].value);
+		var answer = $(".answer");
+		for (x = 0; x < answer.length; x++) {
+			console.log('answer: '+answer[x].value);
+		}});
 });
 
-//fix collapsible btn
-// var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-// var collapseList = collapseElementList.map(function (collapseEl) {
-//   return new bootstrap.Collapse(collapseEl)
-// })
+
